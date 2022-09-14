01fecf5536f5b948feec6322e24f9101974c5c95
CHEREAL <mc.cherdak@mail.ru>
2022-09-15 00:58:39 +0300

    adaptive

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const navBtn = document.querySelector('.nav__tooggle');
+const menuIcon = document.querySelector('.menu-icon');
+
+
+
+navBtn.onclick = function(){
+menuIcon.classList.toggle('menu-icon-active')
+
+
+}
+
