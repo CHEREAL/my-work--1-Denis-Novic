689faf326ed0471ea7854e99b0fbd59b5ca765f2
CHEREAL <mc.cherdak@mail.ru>
2022-09-15 18:14:43 +0300

    the end

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 const navBtn = document.querySelector('.nav__tooggle');
 const menuIcon = document.querySelector('.menu-icon');
-
+const navMobile = document.querySelector('.nav');
 
 
 navBtn.onclick = function(){
 menuIcon.classList.toggle('menu-icon-active')
-
+navMobile.classList.toggle('nav--mobile')
 
 }
 
